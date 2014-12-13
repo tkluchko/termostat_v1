baffab3339221f601a77eacdfc007354115fa74c
ttfaire <ttfaire@mail.ru>
2014-12-13 20:21:55 +0200

    atmega8 algoritm corrected

--- termo.c
+++ termo.c
@@ -3,29 +3,30 @@
 .equ __w1_bit=7
 #endasm
 #include <1wire.h>
-
+#include <stdlib.h>
 #include <mega8.h>
 #include <delay.h>
 
 #include "ds18x20_v3.h"  
 #include <bcd.h>
 
-unsigned char digit_out[4], cur_dig=0;
-
-int temperature;       // то, что возвращает датчик 
-
+//count of ds18b20
 #define MAX_DS18x20 1
-unsigned char ds18x20_devices;
-unsigned char rom_code[MAX_DS18x20][9];
-bit point = 0;
 
+//segments
 #define A   1
-#define B   4
+#define B   128
 #define C   16 
-#define D   64  
-#define E  128 
-#define F  2 
-#define G  8
+#define D   4  
+#define E  2 
+#define F  64 
+#define G  32
+//didits pins 
+#define DIGIT_1  2
+#define DIGIT_2  4
+#define DIGIT_3  8
+#define DIGIT_4  1
+
 
 static flash unsigned char digit[] = {
     0b11111111 ^(A+B+C+D+E+F),   // 0
@@ -45,17 +46,72 @@ static flash unsigned char digit[] = {
     0b11111111 ^(A+D+E+F+G),     // E - 14
     0b11111111 ^(A+E+F+G),       // F - 15
     0b11111111 ^(G),             // 16 - minus
-    0b11111111 ^(A+B+F+G),       // 17 - grad 
+    0b11111111 ^(A+B+F+G),       // 17 - grad?
     0b11111111 ^(0),             // 18 - blank
     0b11111111 ^(C+E+G),         // n
-    0b11111111 ^(D+E+F+G)        // t
+    0b11111111 ^(D+E+F+G),        // t
+    0b11111111 ^(A),        // upper
+    0b11111111 ^(D)        // lower
 };
 
+static flash unsigned char commonPins[] = {
+    DIGIT_1,
+    DIGIT_2,
+    DIGIT_3,
+    DIGIT_4
+};
+//used symbol numbers
 #define MINUS 16
 #define SPACE 18
 #define F_SYMBOL 15
+#define UPPER 21
+#define LOWER 22
+//delays
+#define DELAY 250
 
-    
+#define ENABLE 1
+#define DISABLE 0
+//mode
+#define VIEW_TEMP 0
+#define SET_T1 1
+#define SET_T2 2
+//display limits 
+#define TEMP_1 0
+#define TEMP_2 1
+
+unsigned char digit_out[4], cur_dig=0;
+
+int temperature; 
+
+unsigned char ds18x20_devices;
+unsigned char rom_code[MAX_DS18x20][9];
+bit point = 0;
+
+unsigned char mode = VIEW_TEMP;
+
+int currentTemp;
+
+int temp1=-3;
+int temp2=3;
+
+bit outMode = DISABLE;
+
+void viewTermVar(char showTemp);
+
+// Timer1 overflow interrupt service routine
+interrupt [TIM1_OVF] void timer1_ovf_isr(void) {
+    if(mode == VIEW_TEMP){
+        if (PINC.0==0){ mode = SET_T1;}
+    } else if(mode == SET_T1){ 
+        if (PINC.0==0){ mode = SET_T2; delay_ms(DELAY);}
+        if (PINC.1==0){ temp1--; delay_ms(DELAY);}
+        if (PINC.2==0){ temp1++; delay_ms(DELAY);}
+    } else if(mode == SET_T2){
+        if (PINC.0==0){ mode = VIEW_TEMP; delay_ms(DELAY);}
+        if (PINC.1==0){ temp2--; delay_ms(DELAY);}
+        if (PINC.2==0){ temp2++; delay_ms(DELAY);} 
+    }
+}
 
 // Timer2 overflow interrupt service routine
 interrupt [TIM2_OVF] void timer2_ovf_isr(void) {
@@ -63,53 +119,93 @@ interrupt [TIM2_OVF] void timer2_ovf_isr(void) {
     PORTB=digit[digit_out[cur_dig]];
 
     if (point && cur_dig == 2) {
-        PORTB.5=0;
+        PORTB.3=0;
     }
-    PORTD |= (1<<cur_dig);
+    PORTD |= commonPins[cur_dig];
 	cur_dig++;
     if (cur_dig > 3) {
 		cur_dig = 0;  
     }
 }
 
+void viewTermVar(char showTemp){
+    unsigned char tmp, cur_t=0;
+    bit zero = 0;
+    point = DISABLE;
+
+    if(showTemp == TEMP_1){
+        if (temp1 < 0) zero = 1;
+        digit_out[cur_t++] = LOWER;
+        tmp = abs(temp1);
+       //tmp = abs(currentTemp);
+    }
+    if(showTemp == TEMP_2){
+        if (temp2 < 0) zero = 1;
+        digit_out[cur_t++] = UPPER;
+        tmp = abs(temp2);
+    }
+    if (tmp >= 10) {
+        digit_out[cur_t++] = zero ? MINUS :SPACE ; 
+        digit_out[cur_t++] = tmp / 10;    // ???????
+        digit_out[cur_t++] = tmp % 10;  // ???????
+    } else {
+        digit_out[cur_t++] = SPACE;
+        digit_out[cur_t++] = zero ? MINUS :SPACE ;
+        digit_out[cur_t++] = tmp;      // ???????
+    }
+}
 
 void view_term(void) {
     unsigned char celie, drob, tmp, cur_t=0;
     unsigned int temp, celie_tmp, drob_tmp;
     bit zero;
 
-    // ****************************** начало расчета **********************************
+
     temp = (unsigned int) temperature;
     if (temperature < 0) {
-        temp = ( ~temp ) + 0x0001; // если число отрицательное перевести его в норм.вид
-        zero = 1; //  темп.отрицательная (в дальнейшем атрибут отр.темп.)    
+        temp = ( ~temp ) + 0x0001;
+        zero = 1;    
     } else {
-        zero = 0; // темп.положительная
+        zero = 0;
     }
 
-    celie_tmp = temp >> 4;              // целая часть числа
-    drob_tmp  = temp & 0x000F;          // дробная часть числа с точностью 1/16 градуса
-    drob  = (unsigned char) ((drob_tmp * 10) / 16); // преобразование дробной части в формате "1/16 градуса"
-                                                    // в десятичный формат с точностью 0,1 градуса 
-    celie = (unsigned char) celie_tmp;  // я люблю явное преведение типов
-    if (celie > 99) return; // индикация температуры только до 100 гр.
-    tmp=bin2bcd(celie); // вычислить целую часть
-    // ***************************** конец расчета ************************************
-
+    celie_tmp = temp >> 4;
+    drob_tmp  = temp & 0x000F;
+    drob  = (unsigned char) ((drob_tmp * 10) / 16);
+ 
+    celie = (unsigned char) celie_tmp;
+    if (celie > 99) return;
+    tmp=bin2bcd(celie);
     
+    currentTemp = celie;
+    if(drob >= 5) currentTemp++;
+    if(zero) currentTemp = 0 - currentTemp;
+
+
+    point = ENABLE;
     if (celie >= 10) {
         digit_out[cur_t++] = zero ? MINUS :SPACE ; 
-        digit_out[cur_t++] = tmp >> 4;    // десятки
-        digit_out[cur_t++] = tmp & 0x0F;  // единицы
+        digit_out[cur_t++] = tmp >> 4;
+        digit_out[cur_t++] = tmp & 0x0F;
     } else {
         digit_out[cur_t++] = SPACE;
         digit_out[cur_t++] = zero ? MINUS :SPACE ;
-        digit_out[cur_t++] = tmp & 0x0F;      // единицы
+        digit_out[cur_t++] = tmp & 0x0F;
     }
-    digit_out[cur_t++] = drob; // дробная часть
+    digit_out[cur_t++] = drob;
 }
 
-// Declare your global variables here
+
+void compareTemperature(void){
+    if(outMode == ENABLE && currentTemp > temp2){
+        PORTC.3 = DISABLE;
+        outMode = DISABLE;
+    }
+    if(outMode == DISABLE && currentTemp < temp1){
+        PORTC.3 = ENABLE;
+        outMode = ENABLE;
+    }
+}
 
 void main(void) {
     // Declare your local variables here
@@ -124,8 +220,8 @@ void main(void) {
     // Port C initialization
     // Func6=In Func5=In Func4=In Func3=In Func2=In Func1=In Func0=In 
     // State6=T State5=T State4=T State3=T State2=T State1=T State0=T 
-    PORTC=0x00;
-    DDRC=0x00;
+    PORTC=0xFF;
+    DDRC=0xFF;
 
     // Port D initialization
     // Func7=In Func6=In Func5=In Func4=In Func3=In Func2=In Func1=In Func0=In 
@@ -139,20 +235,21 @@ void main(void) {
     TCCR0=0x00;
     TCNT0=0x00;
 
+
     // Timer/Counter 1 initialization
     // Clock source: System Clock
-    // Clock value: Timer1 Stopped
+    // Clock value: 125,000 kHz
     // Mode: Normal top=0xFFFF
     // OC1A output: Discon.
     // OC1B output: Discon.
     // Noise Canceler: Off
     // Input Capture on Falling Edge
-    // Timer1 Overflow Interrupt: Off
+    // Timer1 Overflow Interrupt: On
     // Input Capture Interrupt: Off
     // Compare A Match Interrupt: Off
     // Compare B Match Interrupt: Off
     TCCR1A=0x00;
-    TCCR1B=0x00;
+    TCCR1B=0x03;
     TCNT1H=0x00;
     TCNT1L=0x00;
     ICR1H=0x00;
@@ -178,7 +275,7 @@ void main(void) {
     MCUCR=0x00;
 
     // Timer(s)/Counter(s) Interrupt(s) initialization
-    TIMSK=0x40;
+    TIMSK=0x44;
 
     // USART initialization
     // USART disabled
@@ -201,7 +298,7 @@ void main(void) {
     // TWI initialization
     // TWI disabled
     TWCR=0x00;
-
+    PORTC.3 = DISABLE;
     w1_init();
 
         
@@ -210,22 +307,45 @@ void main(void) {
     // Global enable interrupts
     #asm("sei")     
 
+
+
     digit_out[0] = SPACE;
     digit_out[1] = SPACE;
     digit_out[2] = F_SYMBOL;
     digit_out[3] = ds18x20_devices;
 
-    point = 1;
-    while (1) {
+
         if (ds18x20_devices >= 1) {
-            for (i=0;i<ds18x20_devices;i++) {
-                if (rom_code[i][0] == DS18B20_FAMILY_CODE){
-                    temperature=ds18x20_temperature(&rom_code[i][0]);
-                }
-                
-                if (temperature!=-9999) view_term();
-                delay_ms(2000);
+        for (i=0;i<ds18x20_devices;i++) {
+            if (rom_code[i][0] == DS18B20_FAMILY_CODE){
+                temperature=ds18x20_temperature(&rom_code[i][0]);
+            }
+                    
+            if (temperature!=-9999){
+                //do nothing 
             }
         }
     }
+    
+    while (1) {   
+        if(mode == VIEW_TEMP){
+            if (ds18x20_devices >= 1) {
+                for (i=0;i<ds18x20_devices;i++) {
+                    if (rom_code[i][0] == DS18B20_FAMILY_CODE){
+                        temperature=ds18x20_temperature(&rom_code[i][0]);
+                    }
+                    
+                    if (temperature!=-9999){
+                        view_term();
+                        compareTemperature();
+                    }
+                }
+            }
+        } else if(mode == SET_T1){ 
+            viewTermVar(TEMP_1);
+        } else if(mode == SET_T2){
+            viewTermVar(TEMP_2);
+        }
+    }
+   
 }
